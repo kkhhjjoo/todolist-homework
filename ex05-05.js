1161c68a50be02bc228cc6f57de064ba5524c8ba
kkhhjjoo <khjoo1203@gmail.com>
2025-10-27 08:41:26 +0900

    순서 맨앞에 추가

--- ex05-05.js
+++ ex05-05.js
@@ -58,7 +58,7 @@ const todoList = [
         <br>
         <textarea placeholder="메모 작성...">${todo.memo || ''}</textarea>
       `;
-      ul.appendChild(li);
+      ul.insertBefore(li,ul.firstChild);
 
       // ✅ 완료 토글
       li.querySelector('.title').addEventListener('click', () => {
